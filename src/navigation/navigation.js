c25c20797acd96cb05d708510a63a29df1b4f9f4
Valentina-Maraio <valentina.maraio89@gmail.com>
2023-06-20 18:46:05 +0200

    navigation completed

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,21 +1,33 @@
-import { NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { Pressable } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { EvilIcons } from "@expo/vector-icons";
 
-import Home from '../screens/Home'
-import Profile from '../screens/Profile'
-import MusicDetails from '../screens/MusicDetails'
+import Home from "../screens/Home";
+import Profile from "../screens/Profile";
+import MusicDetails from "../screens/MusicDetails";
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
-    return(
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Profile" component={Profile} />
-                <Stack.Screen name="MusicDetails" component={MusicDetails} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <Pressable onPress={() => navigation.navigate('Profile')}>
+                <EvilIcons name="user" size={34} />
+              </Pressable>
+            ),
+          })}
+        />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="MusicDetails" component={MusicDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 export default Navigation;
